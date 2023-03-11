8df328be63fe671887823de52a06092be5dbca90
khoroman <aceylacey2002@gmail.com>
2023-03-10 19:21:22 -0500

    added basic wasd controls

---
+++ Assets/Scripts/PlayerCam/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController characterController;
+    public float speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward*z;
+
+        characterController.Move(move* speed * Time.deltaTime);
+
+        
+    }
+}
